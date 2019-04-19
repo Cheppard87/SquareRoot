dfdb821bee2b713b493a845a2710775bdbce06f4
Nad Alaba <nad@alaba.com>
2019-04-19 06:56:25 +0300

    Conflict 1st stage

--- newTrial.cpp
+++ newTrial.cpp
@@ -6,7 +6,7 @@ using namespace std;
 long double sqrtDivision(long double num, int precission = 5) {
     int firshchange;
     int secondchange;
-    int triggeringConflict;
+    int conflict1;
     if (num < 0) num *= -1;
     if (num >= 1000000000000000000) {
         cout << "TOO BIG!";
