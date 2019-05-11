f15ce884cfd96288d515690a61de9d0024de81ae
Nad Alaba <37968805+NadAlaba@users.noreply.github.com>
2019-05-11 16:14:38 +0300

    Added code words and modified limits

--- barcode.cpp
+++ barcode.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <math.h>
 
 using namespace std;
 
@@ -12,30 +14,40 @@ int usedDigitsSum(int digis[], int indexLimit) {
 
 void displayArray(int digis[], int indexLimit) {
     for (int i = 0; i < indexLimit; i++) {
-        cout << digis[i];
+        cout << hex << uppercase << digis[i];
     }
     cout << endl;
 }
 
 int main() {
 
-    int width, r;
-    cout << "(width, r) = "; cin >> width >> r;
+    int width = 17, r = 8;
+    // cout << "width = "; cin >> width;
+    // cout << "r = "; cin >> r;
+    int limit = width - r + 1;
+    limit = 6;
     int digits[r], count = 0;
 
     for (int i = 0; i < r; i++) {
         digits[i] = 1;
     }
+    
+    ios_base::fmtflags f( cout.flags() );
+    std::ofstream out("C:\\Users\\Nadeem\\Desktop\\new.txt");
+    std::streambuf *coutbuf = std::cout.rdbuf(); //save old buf
+    std::cout.rdbuf(out.rdbuf()); //redirect std::cout to out.txt!
 
     int place;
     while (true) {
 
         digits[r-1] = width - usedDigitsSum(digits, r-1);
+        if (digits[r-1] <= limit) {
         displayArray(digits, r);
         count++;
+        }
 
         place = r-2;
-        while ( digits[place] == (width - (r - place - 1) - usedDigitsSum(digits, place)) ) {
+        while ( digits[place] == min(width - (r - place - 1) - usedDigitsSum(digits, place), limit) ) {
             place--;
         }
         if (place < 0) break;
@@ -45,6 +57,19 @@ int main() {
             digits[i] = 1;
         }
     }
-    cout << count << endl;
+    cout.flags( f );
+    
+    // cout << endl;
+    // cout << "width = " << width << endl;
+    // cout << "r = " << r << endl;
+    // cout << "limit = " << limit << endl;
+    // cout << "Total count is " << count << endl;
+    
+    std::cout.rdbuf(coutbuf); //reset to standard output again
+    cout << "width = " << width << endl;
+    cout << "r = " << r << endl;
+    cout << "limit = " << limit << endl;
+    cout << endl << "Total count is " << count << endl;
+
     return 0;
 }
